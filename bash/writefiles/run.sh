9fb5e4b91e7bd94c8ab66b3adf33eeab896174b2
yuvalyacoby <yacoby133@gmail.com>
2022-09-12 18:28:00 +0300

    bash

---
+++ bash/writefiles/run.sh
@@ -0,0 +1,14 @@
+directory='../../typescript/dynamicLoading/versions'
+prefixToRemove='../../typescript/dynamicLoading'
+outputFile='../../typescript/dynamicLoading/testOutput.ts'
+
+for file in "$directory"/*; do
+    fileName=${file##*$prefixToRemove/}
+    fileNoExtension=${fileName%.ts*}
+    if [ "$(cat $outputFile | grep -c $fileNoExtension)" -ge 1 ]
+        then echo "migration for $fileNoExtension already exsits";
+        else
+            echo "export * from './$fileNoExtension';" >> $outputFile
+    fi    
+done
+echo "finished writing all versions to file"
