d220a346583e83d83c0eff1de9e776a9ffca7350
Yuval Yacoby <yacoby133@gmail.com>
2024-11-07 12:43:16 +0200

    outdate it

--- rust/object-store/src/main.rs
+++ rust/object-store/src/main.rs
@@ -33,29 +33,3 @@ async fn main() {
     let r = s3.get(&p).await.unwrap();
     println!("{:#?}", String::from_utf8(r.bytes().await.unwrap().to_vec()));
 }
-
-// localFileSystem
-// #[tokio::main]
-// async fn main() {
-//     let local_disk =
-//         LocalFileSystem::new();
-    
-//     // // list 
-//     // let p = Path::from_filesystem_path(std::path::Path::new("src")).unwrap();
-
-//     // let r: Vec<_> = local_disk.list(Some(&p)).try_collect().await.unwrap();
-//     // println!("{:#?}", r);
-
-//     // put - for local file system, need to create the file first, need to check for S3
-//     // let p = Path::from_filesystem_path(std::path::Path::new("src/file.txt")).unwrap();
-//     // local_disk.put(&p, "some text".into()).await.unwrap();
-
-//     // delete
-//     // let p = Path::from_filesystem_path(std::path::Path::new("src/test/file.txt")).unwrap();
-//     // local_disk.delete(&p).await.unwrap();
-
-//     // get
-//     let p = Path::from_filesystem_path(std::path::Path::new("src/main.rs")).unwrap();
-//     let r = local_disk.get(&p).await.unwrap();
-//     println!("{:#?}", String::from_utf8(r.bytes().await.unwrap().to_vec()));
-// }
